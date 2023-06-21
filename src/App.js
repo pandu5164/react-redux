aadb0f529415604c39d3f7c90fa1f9ce908e57f7
pavan kumar z <zpkplay@gmail.com>
2023-06-21 15:27:35 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import {createStore, applyMiddleware, compose} from "redux";
-import {Provider} from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
 import { ConnectedRouter, routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
 import { createBrowserHistory } from "history";
-import './App.css';
+import "./App.css";
 import routes from "./routes";
 import createRootReducer from "./store/reducers";
 
 export const history = createBrowserHistory();
 export const myRouterMiddlwWare = routerMiddleware(history);
-const logAction = store => { // custom function to applyMiddleware - alternate of thunk
-  return next => {
-    return action => {
+const logAction = (store) => {
+  // custom function to applyMiddleware - alternate of thunk
+  return (next) => {
+    return (action) => {
       const result = next(action);
-      console.log('LogActionResult', result);
+      console.log("LogActionResult", result);
       return result;
-    }
-  }
-}
+    };
+  };
+};
 
-const middlewware = [
-  thunk,
-  myRouterMiddlwWare,
-  logAction
-];
+const middlewware = [thunk, myRouterMiddlwWare, logAction];
 
-const store = createStore(createRootReducer(history), {}, compose(applyMiddleware(...middlewware))); //(createRootReducer(history), applyMiddleware(logAction))
+const store = createStore(
+  createRootReducer(history),
+  {},
+  compose(applyMiddleware(...middlewware))
+); //(createRootReducer(history), applyMiddleware(logAction))
 
 function App() {
   return (
     <Provider store={store}>
-      <ConnectedRouter history={history}> {/* it is used to {routes} as children */}
+      <ConnectedRouter history={history}>
+        {" "}
+        {/* it is used to {routes} as children */}
+        <h1>Counter App</h1>
         {routes}
       </ConnectedRouter>
     </Provider>
